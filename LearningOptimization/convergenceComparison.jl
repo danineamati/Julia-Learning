56d326082a26d08fd8f00d47b75f008db42f0128
Daniel Neamati <danineamati@gmail.com>
2020-06-25 10:40:30 -0700

    PD-AL added and file saving
    Looked at initial results in Excel. But will not include Excel files in the GitHub

--- LearningOptimization/convergenceComparison.jl
+++ LearningOptimization/convergenceComparison.jl
@@ -9,4 +9,43 @@ include("..\\Primal-Dual_IP_Newton_QP\\primal-dual-IP-Newton-QP.jl")
 include("..\\Primal-Dual_AL_Newton_QP\\primal-dual-AL-Newton-QP.jl")
 include("..\\Primal-Dual_AL_Newton_QP\\augmentedLagrangianMethod.jl")
 
+using LinearAlgebra, CSV, DataFrames
+
+# ----------------------------
+# First is setup
+# ----------------------------
+
+println()
+println("Setting Up the QP")
+QMat, cVec, AMat, bVec, x0 = QPSetup(true, true)
+
+fObj(x) = (1/2) * x'QMat*x + cVec'x
+dfdx(x) = QMat * x + cVec
+
+# For the Interior Point Method
+mu = 1
+
+# For the Augmented Lagrangian (including Primal-Dual)
+rho = 1
+nu = zeros(size(AMat, 1))
+lambda = ones(size(AMat, 1))
+
+# φ(x) = f(x) + (ρ/2) c(x)'c(x) + λ c(x)
+phiFun(x) = fObj(x) + (rho / 2) * cPlus(AMat, x, bVec)'cPlus(AMat, x, bVec) +
+                lambda' * cPlus(AMat, x, bVec)
+dPhidx(x) = getQPgradPhiAL(x, QMat, cVec, AMat, bVec, rho, lambda)
+
+println("Objective: f(x) = (1/2) x'Qx + c'x")
+println("Q = $QMat")
+println("c = $cVec")
+println("Constraints: Ax ≤ b")
+println("A = $AMat")
+println("b = $bVec")
+println("Initial Starting Point: $x0")
+
+xStates = pdALNewtonQPmain(QMat, cVec, AMat, bVec, x0, lambda, rho, nu,
+                fObj, dfdx, 0.1, 0.5, false)
+
+CSV.write("testSave.csv", DataFrame(xStates), writeheader = false)
+
 println("Complete")
