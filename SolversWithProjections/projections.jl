e7fa6876a2125237c151d97e8365c9e2020d130f
Daniel Neamati <danineamati@gmail.com>
2020-06-29 20:55:40 -0700

    Added more general affine projections

--- SolversWithProjections/projections.jl
+++ SolversWithProjections/projections.jl
@@ -1,12 +1,24 @@
 
 
+# Covers how to consider a square violation
+function sqrDistEuclid(pt, projFunc)
+    #=
+    Choose a given projection method and determine the squared distance
+    at this point.
+    =#
+    return norm(pt - projFunc(pt), 2)^2
+end
+
+
+
 # Covers Projections
 
+# First, Lines from vectors at the origin
 function projLine(pt, vec)
     #=
     This function projects a point pt onto a line described by vec
 
-    proj_v(p) = v * (p.v)/(v.v)
+    proj_v(p) = v * (p'v)/(v'v)
     =#
     return vec * (pt'vec) / (vec'vec)
 end
@@ -16,13 +28,13 @@ function orthoProjLine(pt, vec)
     This function projects a point pt onto the orthogonal of a line described
     by vec
 
-    proj_v(p) = v * (p.v)/(v.v)
-    proj_v⟂(p) = pt - v * (p.v)/(v.v)
+    proj_v(p) = v * (p'v)/(v'v)
+    proj_v⟂(p) = pt - v * (p'v)/(v'v)
     =#
     return pt - projLine(pt, vec)
 end
 
-
+# Second, Positive Orthant
 function projPosOrth(pt)
     #=
     This function is for a constraint of the form "τ ≥ 0."
@@ -35,11 +47,34 @@ function projPosOrth(pt)
     return max.(pt, 0)
 end
 
+# Third, Affine Case
+function projAffineEq(a, b, x)
+    #=
+    Projection for Affine Equality Case (a'x = b)
 
-# Projection for Affine Equality Case (Ax = b)
+    We can show that the projection is given by
+    proj(x) = x + (b - a'x) * a / a'a
+            = x + a * [b] / a'a - a * (a'x) / a'a
+    =#
+    t1 = a * b / a'a
+    t2 = a * (a'x) / a'a
+    return x + t1 - t2
+end
 
 
-# Projection for Affine Inequality Case (Ax ≤ b)
+function projAffineIneq(a, b, x)
+    #=
+    Projection for Affine Inequality Case (a'x ≤ b)
+
+    We can show that the projection is given by
+    projAffineEq(x) for a'x > b
+    x               for a'x ≤ b
+    =#
+    if a'x > b
+        return projAffineEq(a, b, x)
+    end
+    return x
+end
 
 
 # Projection for Second-Order Cone
--- SolversWithProjections/testProjectionsWithPlots.jl
+++ SolversWithProjections/testProjectionsWithPlots.jl
@@ -5,17 +5,57 @@ gr()
 
 include("projections.jl")
 
-# First, we plot a line and the distance from the line
+
+# First, we plot a line and the squared distance from the line
 xRange = -10:0.1:10
 yRange = -8:0.1:8
 vecTest = [5; 4]
 lineTest(x) = (vecTest[2]/vecTest[1]) * x
 
-distanceSqrLine(x, y) = norm(orthoProjLine([x; y], vecTest))^2
+# distanceSqrLine(x, y) = norm(orthoProjLine([x; y], vecTest))^2
+distanceSqrLine(x, y) = sqrDistEuclid([x; y], pt -> projLine(pt, vecTest))
 
 plt = plot(xRange, lineTest, label = "Line Constraint")
 contour!(xRange, yRange, distanceSqrLine, label = "Sqr Distance to Line")
 title!("Squared of Distance to Line")
+xlabel!("X")
+ylabel!("Y")
+display(plt)
+
+
+# Second, we plot the distance from the positive orthant
+distSqrPosOrth(x, y) = sqrDistEuclid([x; y], pt -> projPosOrth(pt))
+
+rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
+
+plt2 = plot(rectangle(10,8,0,0), opacity = 0.25, label = "Positive Orthant")
+contour!(xRange, yRange, distSqrPosOrth, label = "Sqr Distance to Line")
+title!("Squared of Distance to Positive Orthant")
+xlabel!("X")
+ylabel!("Y")
+display(plt2)
+
+
+# Third, we plot the squared distance from an affine equality constraint
+# a'x = b -> a1 x + a2 y = b
+# So y = (- a1 / a2) * x + (b / a2)
+# Note that in the form a'x = b,
+# a is the "normal vector to the hyperplane"
+# b is the "offset"
+aTest = [5; 4]
+bTest = 4 * aTest[2]
+
+xRange = -10:0.1:10
+yRange = -9:0.1:17
+
+affineTest(x) = (-aTest[1]/aTest[2]) * x + bTest / aTest[2]
+distSqrAffine(x, y) = sqrDistEuclid([x; y],
+                            pt -> projAffineIneq(aTest, bTest, [x; y]))
+
+plt = plot(xRange, affineTest, label = "Affine Constraint",
+                        fill = (minimum(yRange), 0.25, :auto))
+contour!(xRange, yRange, distSqrAffine, label = "Sqr Distance to Line")
+title!("Squared of Distance to Affine Inequality Constraint")
 xlabel!("X")
 ylabel!("Y")
 display(plt)
