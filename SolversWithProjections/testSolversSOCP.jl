55fa7121ea9cded0e1833a6fe2e9b26c1cecdeb0
Daniel Neamati <danineamati@gmail.com>
2020-07-10 15:06:04 -0700

    It seems that the current region is impossible to solve?
    Working through the visualization

--- SolversWithProjections/testSolversSOCP.jl
+++ SolversWithProjections/testSolversSOCP.jl
@@ -8,6 +8,10 @@ include("runSOCP-Setup.jl")
 
 yStates, res = ALPrimalNewtonSOCPmain(y0, alcone, currSolveParams, false)
 
+#=
+Get and plot path
+=#
+
 xVecs = getXVals(yStates)
 xVals = [x[1] for x in xVecs]
 yVals = [x[2] for x in xVecs]
@@ -17,6 +21,10 @@ title!("Solver Path")
 xlabel!("X")
 ylabel!("Y")
 display(plt)
+
+#=
+Calculate and plot the constraint violation
+=#
 
 cV, aV, lV = getViolation(yStates, alcone.constraints)
 
@@ -29,5 +37,31 @@ title!("Constraint Violation")
 xlabel!("Newton Step")
 ylabel!("Violation")
 display(pltC)
+
+#=
+Calculate and plot the residuals
+=#
+alclean = augLagQP_2Cone(thisQP, thisConstr, currSolveParams.penaltyMax,
+                                zeros(lambdaSize))
+
+sTest = [[0;0;0], s0, [20; 20; 20]]
+tTest = [0, t0, 10]
+
+for (si, sIter) in enumerate(sTest)
+    for (ti, tIter) in enumerate(tTest)
+        merit(x, y) = evalAL(alclean, SOCP_primals([x; y], sIter, tIter))
+
+        xRange = -15:1:10
+        yRange = -6:1:10
+
+        contoursMerit = contour(xRange, yRange, merit)
+        title!("Contour Plots of the Merit Function\n" *
+                "With s = $sIter and t = $tIter")
+        xlabel!("X")
+        ylabel!("Y")
+        display(contoursMerit)
+        savefig(contoursMerit, "contoursMerit-$si-$ti")
+    end
+end
 
 println("Tests Complete")
