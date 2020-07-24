df0e6f09756d6cd0cbbca33650cb01682e071450
Daniel Neamati <danineamati@gmail.com>
2020-07-24 15:07:28 -0700

    Added many docstrings for the SOCP

--- SolversWithProjections/SOCP-Setup-Simple.jl
+++ SolversWithProjections/SOCP-Setup-Simple.jl
@@ -18,21 +18,49 @@ using LinearAlgebra
 
 include("constraints.jl")
 
-# Instead of the function below, use the "isposdef" function in the
-# LinearAlgebra Package.
-
-# function checkPosDef(Q)
-#     # To check Positive Definite, we check that the eigenvalues are positive
-#     # and that the determinant is positive.
-#     eVals = eigvals(Q)
-#     numRows = size(Q, 1)
-#     return (eVals > vec(zeros(numRows, 1))) && (det(Q) > 0)
-# end
 
 # -------------------------
 # Solver Parameters
 # -------------------------
+"""
+Line Search Parameters (Notation from Toussaint Notes (2020))
 
+paramA::Float16         # Used in Line Search, should be [0.01, 0.3]
+
+paramB::Float16         # Used in Line Search, should be [0.1, 0.8]
+
+----
+Iteration Counter Parameters
+
+maxOuterIters::Int32    # Number of Outer Loop iterations
+
+maxNewtonSteps::Int32   # Number of Newton Steps per Outer Loop iterations
+
+----
+Exit Condition Parameter
+
+rTol::Float64           # When steps are within rTol, loop will stop.
+
+----
+Penalty Update Parameters
+
+penaltyStep::Float16    # Multiplies the penalty parameter per outer loop
+
+penaltyMax::Float64     # Maximum value of the penalty parameter
+
+----
+Trust Region Parameters (Notation from Nocedal et Yuan (1998))
+
+trSizeStart::Float32    # Starting size of the trust region
+
+trc1::Float16           # Success Size Increase Parameter (1 < c1)
+
+trc2::Float16           # Taylor Series Error Parameter (0 < c2 < 1)
+
+trc3::Float16           # Failed Size Reduction Parameter (0 < c3 < c4)
+
+trc4::Float16           # Failed Size Reduction Parameter (c3 < c4 < 1)
+"""
 struct solverParams
     # Line Search Parameters (Notation from Toussaint Notes (2020))
     paramA::Float16         # Error allowed should be [0.01, 0.3]
@@ -53,6 +81,11 @@ struct solverParams
     trc4::Float16           # Failed Size Reduction Parameter (c3 < c4 < 1)
 end
 
+"""
+Prints the struct variables in the `solverParams` struct.
+
+See also: [`solverParams`](@ref) for more information.
+"""
 function solParamPrint(sp::solverParams)
     println()
     println("Beginning solver with parameters: ")
@@ -70,54 +103,93 @@ function solParamPrint(sp::solverParams)
     println("$(sp.trc3 < sp.trc4) & $(sp.trc4 < 1)")
 end
 
-# ---------------------------
-# Objective Function
-# (1/2) xT Q x + cT x
-# Q is an nxn Symmetric Matrix (that is positive definite for convex)
-# c is an nx1 vector
-# --------------------------
-"""
-Objective Function
+@doc raw"""
+Quadratic Objective/Cost Function
 
-(1/2) xT Q x + cT x
+    objectiveQP(Q, p)
+----
 
-Q is an nxn Symmetric Matrix (that is positive definite for convex)
+```math
+\frac{1}{2} x^{⊤} Q x + p^{⊤} x
+```
 
-c is an nx1 vector
+`Q` is an nxn Symmetric Matrix (that is positive definite for convex)
+
+`p` is an nx1 vector
+
+See also: [`fObjQP`](@ref), [`dfdxQP`](@ref)
 """
 struct objectiveQP
+    "Positive Definite Symmetric Matrix"
     Q
     p
 end
 
 # Input x as a COLUMN vector (i.e. x = [4; 3])
+"""
+    fObjQP(qp::objectiveQP, x)
+
+Evaluates a quadratic function at the input `x`. The input `x` should be a
+*column* vector (i.e. x = [4; 3])
+"""
 function fObjQP(qp::objectiveQP, x)
     return (1/2) * x' * (qp.Q) * x + (qp.p)' * x
 end
 
+@doc raw"""
+    dfdxQP(qp::objectiveQP, x)
+
+Evaluates the derivative of a quadratic function at input `x`.
+
+```math
+\frac{d}{dx} (\frac{1}{2} x^{⊤} Q x + p^{⊤} x) = Qx + p
+```
+"""
 function dfdxQP(qp::objectiveQP, x)
     return (qp.Q) * x + qp.p
 end
 
-# ------------------------
-# We have three primal variables: [x, s, t]
-# We can stack them in one struct
-# ------------------------
+"""
+    SOCP_primals(x, s, t)
 
+Struct to store the primal variables of the form [x; s; t]
+
+`x` and `s` are mx1 and nx1 vectors.
+`t` is a real number
+
+See also: [`primalVec`](@ref) and [`primalStruct`](@ref)
+"""
 mutable struct SOCP_primals
     x
     s
     t
 end
 
+"""
+    primalVec(y::SOCP_primals)
+
+Converts [`SOCP_primals`](@ref) struct to [x; s; t] vector.
+"""
 function primalVec(y::SOCP_primals)
     return [y.x; y.s; y.t]
 end
 
+"""
+    primalStruct(v, xSize::Int64, sSize::Int64, tSize::Int64)
+
+Converts [x; s; t] vector to [`SOCP_primals`](@ref) struct
+"""
 function primalStruct(v, xSize::Int64, sSize::Int64, tSize::Int64)
     return SOCP_primals(v[1:xSize], v[xSize+1:xSize+sSize], v[end])
 end
 
+
+"""
+    getXVals(yList::Array{SOCP_primals, 1})
+
+Returns the "x" values of and an array of [`SOCP_primals`](@ref).
+Used primarily with plotting.
+"""
 function getXVals(yList::Array{SOCP_primals, 1})
     xList = []
     for xst in yList
@@ -126,6 +198,34 @@ function getXVals(yList::Array{SOCP_primals, 1})
     return xList
 end
 
+@doc raw"""
+    getViolation(yList::Array{SOCP_primals, 1}, c::AL_coneSlack)
+
+returns how much each SOCP constraint is violated
+(in 3 mulitdimensional arrays).
+
+Recall that an SOCP is generally
+```math
+||Ax-b|| ≤ c^⊤ x - d
+```
+
+Using slack variables, we have
+
+```math
+||s|| - t ≤ 0
+```
+
+```math
+(Ax - b) - s = 0
+```
+
+```math
+(c^⊤ x - d) - t = 0
+```
+
+This function returns the constraint violation for each of the above 3 in 3
+separate arrays.
+"""
 function getViolation(yList::Array{SOCP_primals, 1}, c::AL_coneSlack)
     coneViolation = []
     affineViolation = []
@@ -149,6 +249,25 @@ end
 # lambdaInit = zeros(size(fObj(x0)))
 # rhoInit = 1
 
+@doc raw"""
+    augLagQP_2Cone(obj::objectiveQP, constraints::AL_coneSlack, ρ, λ)
+
+This is a mutable struct that stores an SOCP Augmented Lagrangian.
+
+For a single SOCP constraint, the lagrangian is
+```math
+φ(y) = f(x) + (ρ/2) ||c(y)||_2^2 + λ c(y)
+```
+```math
+φ(y) = f(x) + (ρ/2) c(y)'c(y)    + λ c(y)
+```
+
+Where `y = [x; s; t]` is represented as an [`SOCP_primals`] struct.
+
+Generally, we initialize `λ` to 0 and `ρ` to 1.
+
+See also: [`evalAL`](@ref), [`evalGradAL`](@ref), [`evalHessAl`](@ref)
+"""
 mutable struct augLagQP_2Cone
     obj::objectiveQP
     constraints::AL_coneSlack # Currently the "p-cone" is only a "2-cone"
@@ -156,6 +275,13 @@ mutable struct augLagQP_2Cone
     lambda
 end
 
+"""
+    evalAL(alQP::augLagQP_2Cone, y::SOCP_primals)
+
+Evaluates the Augmented Lagrangian of an SOCP with the primals `y`
+
+returns a real number
+"""
 function evalAL(alQP::augLagQP_2Cone, y::SOCP_primals)
     # φ(y) = f(x) + (ρ/2) c(y)'c(y)    + λ c(y)
     # φ(y) = [(1/2) xT Q x + cT x] + (ρ/2) (c(y))'(c(y)) + λ (c(y))
@@ -165,6 +291,14 @@ function evalAL(alQP::augLagQP_2Cone, y::SOCP_primals)
     return fCurr + (alQP.rho / 2) * cCurr'cCurr + (alQP.lambda)' * cCurr
 end
 
+"""
+    evalGradAL(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
+
+Evaluates the gradient of the Augmented Lagrangian of an SOCP with the
+primals `y`
+
+returns a vector of size `y`
+"""
 function evalGradAL(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
     # ∇φ(y) = ∇f(x) + J(c(y))'(ρ c(y) + λ)
     # y = [x; s; t]
@@ -210,6 +344,14 @@ function evalGradAL(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
     return paddedGradf + cTotal
 end
 
+"""
+    evalHessAl(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
+
+Evaluates the Hessian of the Augmented Lagrangian of an SOCP with the
+primals `y`
+
+returns a Symmetric Matrix of size `y`x`y`
+"""
 function evalHessAl(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
     #=
     H(φ(x)) = H(f(x)) + ((ρ c(x) + λ) H(c(x)) + ρ ∇c(x) * ∇c(x))
@@ -236,15 +378,32 @@ function evalHessAl(alQP::augLagQP_2Cone, y::SOCP_primals, verbose = false)
     return hessfPadded + alQP.rho * hesscCurr
 end
 
+"""
+    getNormRes(resArr, floor = 10^(-20))
 
+Acts as a safe 2-norm that operates on an array that would approach zero.
+Usually this is used with semilog or log-log plots of the residuals
+
+See also: [`safeNorm`](@ref), [`calcNormGradResiduals`](@ref)
+"""
 function getNormRes(resArr, floor = 10^(-20))
     # Take the norm to get non-negative.
     # Take max with floor to get positive.
     return max.(norm.(resArr), floor)
 end
 
+"""
+    calcNormGradResiduals(alQP::augLagQP_2Cone,
+                          yList::Array{SOCP_primals, 1})
+
+Calculates the 2-norm of the residuals of an augmented lagrangian at each of
+the inputs `y`. The 2-norm in safe in that it is bounded below (e.g. by
+10^(-20)) and can be plotted on a semilog or log-log plot.
+
+See also: [`getNormRes`](@ref)
+"""
 function calcNormGradResiduals(alQP::augLagQP_2Cone,
-                                    yList::Array{SOCP_primals, 1})
+                               yList::Array{SOCP_primals, 1})
     #=
     Calculate the residuals where the AL is the merit function.
     Returns the norm of the gradient of the AL at each point in xArr
@@ -253,6 +412,11 @@ function calcNormGradResiduals(alQP::augLagQP_2Cone,
     return getNormRes(resArr)
 end
 
+"""
+    calcALArr(alQP::augLagQP_2Cone, yList::Array{SOCP_primals, 1})
+
+Evaluates the augmented lagrangian at each input `y`.
+"""
 function calcALArr(alQP::augLagQP_2Cone, yList::Array{SOCP_primals, 1})
     #=
     Calculate the value of the Augmented Lagrangian at each point
