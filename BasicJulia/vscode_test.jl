605e79db34311c8730438ac9824b27b06e223767
Daniel Neamati <danineamati@gmail.com>
2020-09-08 10:46:15 -0700

    Attempted VS Code
    Unsure how to get the debugger working.

---
+++ BasicJulia/vscode_test.jl
@@ -0,0 +1,16 @@
+# Test Julia on VSCode
+
+println("Hello World, from VSCode")
+
+a = 5
+println("a = $a")
+
+a = [4,5,6]
+println("now, a = $a, or more cleanly: a =")
+display(a)
+
+for i in 1:1000
+    println("Counter $i")
+end
+
+println("\nComplete")
